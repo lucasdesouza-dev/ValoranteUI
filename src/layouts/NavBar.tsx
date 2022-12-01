ee49e7d0a694035f7bfb21e4cf95218cf1bf1f0a
Lucas Henrique de Souza <55006206+lucasdesouza-dev@users.noreply.github.com>
2022-12-01 10:07:32 -0300

    rota icon

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -34,7 +34,9 @@ const NavBar = () => {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+               <Link to={"/"}> <div className="flex flex-shrink-0 items-center">
+                 
+                 
                   <img
                     className="block h-8 w-auto lg:hidden"
                     src="https://logodownload.org/wp-content/uploads/2020/06/valorant-logo-1.png"
@@ -46,6 +48,7 @@ const NavBar = () => {
                     alt="Your Company"
                   />
                 </div>
+                </Link>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4 ">
                     {navigation.map((item) => {
